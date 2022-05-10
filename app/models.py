cf8a60dbeefff1f73a824a3b7fd3b6cf3f20f336
Machuka Joy <machukajoy@gmail.com>
2022-05-10 20:40:26 +0300

    update database

--- app/models.py
+++ app/models.py
@@ -5,9 +5,19 @@ from werkzeug.security import generate_password_hash,check_password_hash
 class User(db.Model,UserMixin):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(255),index = True)
-    password = db.Column(db.String(255))
+    username = db.Column(db.String(255),unique = True,index = True)
     email  = db.Column(db.String(255),unique = True,index = True)
+    secure_password = db.Column(db.String(255))
+
+    @property
+    def password(self):
+        raise AttributeError("You can not read the password")
+    @password.setter
+    def password(self, password):
+        self.secure_password = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.secure_password,password) 
 
     def save(self):
         db.session.add(self)
@@ -17,21 +27,5 @@ class User(db.Model,UserMixin):
         db.session.delete(self)
         db.session.commit()
 
-    @property
-    def password(self):
-        raise AttributeError("You can not read the password")
-    @password.setter
-    def set_password(self, password):
-        password_hash = generate_password_hash(password)
-        self.password = password_hash
-
-    def verify_password(self, password):
-        return check_password_hash(self.password,password) 
-
-
     def __repr__(self):
         return f'User {self.username}'
-
-@login_manager.user_loader
-def user_loader(user_id):
-    return User.query.get(user_id)
