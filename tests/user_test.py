cb16f8183f871d1bb33df7918ab2022d61f8e235
Machuka Joy <machukajoy@gmail.com>
2022-05-10 18:54:30 +0300

    ts: add test cases for user

--- tests/user_test.py
+++ tests/user_test.py
@@ -4,7 +4,14 @@ from app.models import User
 
 class UserTest(unittest.TestCase):
     def setUp(self):
-        self.new_user = User(username='charles',password='qwerty')
+        self.new_user = User(username='machuka',password='p@ssword')
 
     def test_password_setter(self):
         self.assertTrue(self.new_user.password is not None)
+
+    def test_no_access_password(self):
+        with self.assertRaises(AttributeError):
+            self.new_user.password
+
+    def test_password_verification(self):
+        self.assertTrue(self.new_user.verify_password('p@ssword'))
