170a0dfa525d8a9ccec825e7691a7c3f875b70fb
Machuka Joy <machukajoy@gmail.com>
2022-05-11 10:17:54 +0300

    ft(database): add a table to hold pitches to our database

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from . import db, login_manager
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash,check_password_hash
+from datetime import datetime
 
 class User(db.Model,UserMixin):
     __tablename__ = 'users'
@@ -8,9 +9,9 @@ class User(db.Model,UserMixin):
     username = db.Column(db.String(255),unique = True,nullable = False)
     email  = db.Column(db.String(255),unique = True,nullable = False)
     secure_password = db.Column(db.String(255),nullable = False)
-
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
+    pitches = db.relationship('Pitch', backref='users', lazy='dynamic')
 
     @property
     def set_password(self):
@@ -34,6 +35,16 @@ class User(db.Model,UserMixin):
 
     def __repr__(self):
         return f'User {self.username}'
+class Pitch(db.Model):
+    __tablename__ = 'pitches'
+    id = db.Column(db.Integer, primary_key = True)
+    post = db.Column(db.Text())
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    time = db.Column(db.DateTime, default = datetime.utcnow)
+    category = db.Column(db.String(255), index = True)
+    def save_p(self):
+        db.session.add(self)
+        db.session.commit()
 
 @login_manager.user_loader
 def load_user(user_id):
